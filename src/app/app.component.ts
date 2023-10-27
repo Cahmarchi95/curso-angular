dc42f9454b80475e4a1a1d50fa91548d7dd8d5ae
Cahmarchi95 <cah.de.marchi@hotmail.com>
2023-10-27 09:55:59 -0300

    feat: exercícios conceitos de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  userName: string = "Joaquim";
-  age = "32";
+  userName: string = 'Joaquim';
+  age = '32';
   userData = {
-    email:"joaquim@email.com",
-    role: "admin"
-  }
+    email: 'joaquim@email.com',
+    role: 'admin',
+  };
   title = 'curso-angular';
 }
